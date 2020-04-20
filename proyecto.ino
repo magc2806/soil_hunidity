34dc67329adfeb5606c6e001fb943441022e1fe5
Manuel Gonzalez <gonzalezcabellomanuel@gmail.com>
2020-04-20 00:48:59 -0400

    Integrado configuracion de modulo GSM. De momento no envia con datos queda pendiente revisar el por que

--- proyecto.ino
+++ proyecto.ino
@@ -5,14 +5,28 @@
 #include <LiquidCrystal_I2C.h>
 
 
+#define TINY_GSM_MODEM_SIM800 // definição do modem usado (SIM800L)
+#define TINY_GSM_RX_BUFFER   1024
+#include <TinyGsmClient.h> // biblioteca com comandos GSM
 
-//*********Conexión Wifi************
+//---------GSM-----------
+HardwareSerial SerialGSM(2); //Uso de modulo UART2 para conexion con el modulo GSM
+TinyGsm modemGSM(SerialGSM); // Objeto TinyGsm
+const int BAUD_RATE = 9600; // Velocidad de comunicacion
+const int RX_PIN = 16, TX_PIN = 17; //Pines para conectar al modulo GSM
+
+//Credenciales para conectar datos de SIM. Valido para ENTEL 
+const char apn[]  = "imovil.entelpcs.cl";
+const char gprsUser[] = "entelpcs";
+const char gprsPass[] = "entelpcs";
+
+//---------Conexión Wifi------------
 #define WIFI_SSID "Familia Costanera"
 #define PASSWORD "Costanera728"
 
 // ----PARAMETROS DE WEB---
-#define TOKEN  "d771ba30a89855600bfc13c1d1d6377d"
-#define WIFI_SSID "Familia Costanera"                                             // input your home or public wifi name
+#define TOKEN  "d771ba30a89855600bfc13c1d1d6377d" //Token de la API
+#define WIFI_SSID "Familia Costanera"                                   // input your home or public wifi name
 #define WIFI_PASSWORD "Costanera728"                                    //password of wifi ssid
 String website_URL = "http://evening-headland-87613.herokuapp.com/api/v1/measurements";
 int measurement_point_id=1;
@@ -33,7 +47,7 @@ unsigned long last_sending_time = 0;
 
 
 
-//---LCD----
+//---LCD---- SDA ES GPIO 21 SCL GPIO22
 const int lcdColumns = 16;
 const int lcdRows = 2;
 //Objeto LCD
@@ -54,12 +68,35 @@ void show_to_lcd(){
       sprintf(data, "Hum: %d", humidity_value);
       lcd.print(data);   
            
-      }
+  }
+
+  
+void setup_gsm(){
+  Serial.println("Starting...");
+  SerialGSM.begin(BAUD_RATE, SERIAL_8N1, RX_PIN, TX_PIN, false);
+  delay(3000);
+  Serial.println(modemGSM.getModemInfo());
+  delay(100);
+  if (!modemGSM.restart()) {
+  // if (!modem.init()) {
+    Serial.println(F(" Restarting"));
+    
+    delay(100);
+    return;
+  }
+
+  if (!modemGSM.gprsConnect(apn, gprsUser, gprsPass)) {
+    Serial.println("Connection failed");
+    }
+    else{
+      Serial.println("Connected !!!");
+    }  
+  }
 
 
 void sending_data(){
 
-  if (WiFi.status() == WL_CONNECTED) { //Check WiFi connection status
+  if (modemGSM.gprsConnect(apn, gprsUser, gprsPass)) { //Check WiFi connection status
  
     StaticJsonBuffer<400> jsonBuffer;
     JsonObject& jsonObj = jsonBuffer.createObject();
@@ -91,7 +128,8 @@ void sending_data(){
    }
    else {
  
-    Serial.println("Error in WiFi connection");
+    //Serial.println("Error in WiFi connection");
+    Serial.println("Error in Data connection");
  
    }
 
@@ -104,7 +142,7 @@ void setup() {
   WiFi.mode(WIFI_STA); //Configuro el ESP32 como station mode para que se pueda conectar a alguna red
   WiFi.disconnect(); 
 
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
   WiFi.begin(WIFI_SSID, PASSWORD);
   pinMode(humidity_pin, INPUT);
   pinMode(led, OUTPUT);
@@ -115,7 +153,7 @@ void setup() {
  * Si esta conectado debería retornar 3
  */
   while (WiFi.status() != WL_CONNECTED ){ // mientras no este coenctado envío mensaje de que no se puede conectar a la red
-       Serial.print("No se puede conectar a la red");
+       Serial.println("No se puede conectar a la red");
        delay(500); 
        digitalWrite(led, LOW);
        delay(1000);
@@ -126,6 +164,8 @@ void setup() {
   if(WiFi.status()== WL_CONNECTED){
     digitalWrite(led, HIGH);
   }
+
+    
   
   lcd.init();                                                     //INICIO LCD
   delay(10);
@@ -133,17 +173,28 @@ void setup() {
   lcd.backlight();                                                //Enciendo la luz trasera del LCD
   lcd.setCursor(0,0);
   lcd.print("Sin medida");
-
-  
+  setup_gsm(); 
+  WiFi.disconnect(); 
+  Serial.println("Wifi disconnected");
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  lcd.print("loop");  
 
   while(!Serial){
-    //Me quedo aquí mientras no haya comunicación serial    
+    //Me quedo aquí mientras no haya comunicación serial 
+    lcd.print("Esperando comunicacion serial");   
     
     }
+
+    if (!modemGSM.gprsConnect(apn, gprsUser, gprsPass)) {
+    Serial.println("Connection failed");
+    }
+    else{
+      Serial.println("Connected !!!");
+    }  
+    
 
     current_time= millis();
     //Muestra en LCD
